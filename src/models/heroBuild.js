301d7b89a08d0d8a70b543ebe6b2a46e80743809
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-09-05 15:34:44 -0400

    create schema for build creator

--- src/models/heroBuild.js
+++ src/models/heroBuild.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const Schema = mongoose.Schema;
+
 mongoose.connect('mongodb://localhost:27017/fhdb');
+
+const buildSchema = new Schema({
+  name: { type: String, required: true },
+  user_id: { type: Number, required: true },
+  feats: [{ type: Array, 'default': [] }],
+  feats: [{ type: Array, 'default': [] }],
+  summary: { type: String, required: true }
+});
