0ca8be55b9c64a66e9ce7982c77c92aa7522f0a0
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-09-13 21:48:10 -0400

    create form not exact but I plan to use a react form with more detail on build

--- src/components/heroBuildForm/index.js
+++ src/components/heroBuildForm/index.js
@@ -10,6 +10,16 @@ class heroBuildForm extends Component {
     return (
       <div>
 
+<form >
+First name:<br>
+<input type="text" name="firstname" value="Mickey">
+<br>
+Last name:<br>
+<input type="text" name="lastname" value="Mouse">
+<br><br>
+<input type="submit" value="Submit">
+</form>
+
       </div>
 
     );
