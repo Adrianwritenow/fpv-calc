770c020ae895fd74248bed90822ca0538c3f9854
Adrian Rodriguez <adrianwritenow@gmail.com>
2018-09-22 11:46:37 -0400

    the test perk object passes through centurion hero object so i will map out rest of hero perks through heroes

--- src/components/characters/knights/centurion.js
+++ src/components/characters/knights/centurion.js
@@ -1,4 +1,5 @@
 import feats from '../../feats';
+import perks from '../../perks';
 
 export default{
   name:'Centurion',
@@ -8,5 +9,13 @@ export default{
   feats:[feats.level1.ironLungs,feats.level1.bountyHunter,feats.level1.rush,
           feats.level2.inspire,feats.level2.haymaker,feats.level2.centurionsMarch,
           feats.level3.secondWind,feats.level3.pugio,feats.level3.sharpenBlade,
-          feats.level4.catapult,feats.level4.stalwartBanner,feats.level4.phalanx]
+          feats.level4.catapult,feats.level4.stalwartBanner,feats.level4.phalanx],
+  perks:{
+    common:[perks.galestorm,perks.devourer,perks.earlyReaper],
+    rare:perks.endurance,
+    heroic:perks.survivalInstinct,
+    epic:perks.crushThem,
+    legendary:perks.headHunter
+  }
+
 }
